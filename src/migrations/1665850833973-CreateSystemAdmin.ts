22ff125ea6053c448de71d53e247baa3eb088d2e
thongdk8 <thong8111996@gmail.com>
2022-10-16 11:51:23 +0900

    update migration

--- src/migrations/1665850833973-CreateSystemAdmin.ts
+++ src/migrations/1665850833973-CreateSystemAdmin.ts
@@ -1,59 +1,23 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as bcryptjs from 'bcryptjs';
 
 export class CreateSystemAdmin1665850833973 implements MigrationInterface {
   name = 'CreateSystemAdmin1665850833973';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "FK_a50386174b5d9b1a258f2332cce"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "FK_3f916f97edcba428ad50d8d54ef"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "salary"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "name"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "address"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "phone"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "UQ_a50386174b5d9b1a258f2332cce"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "company_id"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "UQ_3f916f97edcba428ad50d8d54ef"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "company"');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "company" uuid');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD CONSTRAINT "UQ_3f916f97edcba428ad50d8d54ef" UNIQUE ("company")');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "salary" integer NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "name" character varying NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "address" character varying NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "phone" character varying NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "company_id" uuid');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD CONSTRAINT "UQ_a50386174b5d9b1a258f2332cce" UNIQUE ("company_id")');
-    await queryRunner.query(
-      'ALTER TABLE "company_admin" ADD CONSTRAINT "FK_3f916f97edcba428ad50d8d54ef" FOREIGN KEY ("company") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
-    );
-    await queryRunner.query(
-      'ALTER TABLE "company_admin" ADD CONSTRAINT "FK_a50386174b5d9b1a258f2332cce" FOREIGN KEY ("company_id") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
-    );
+    const systemAdminEmailName = 'system@email.com';
+    const systemAdminPassword = bcryptjs.hashSync('password', 10);
+    const query =
+      `INSERT INTO public."user" (id, name, email, password, role, created_at, updated_at) VALUES ('2b510726-e10c-4efc-a2c9-6df536382ecb', 'string', '` +
+      systemAdminEmailName +
+      `', '` +
+      systemAdminPassword +
+      `', 'system_admin', '2022-10-15 17:05:23.725461', '2022-10-15 17:05:23.725461');`;
+    await queryRunner.query(query);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "FK_a50386174b5d9b1a258f2332cce"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "FK_3f916f97edcba428ad50d8d54ef"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "UQ_a50386174b5d9b1a258f2332cce"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "company_id"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "phone"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "address"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "name"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "salary"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP CONSTRAINT "UQ_3f916f97edcba428ad50d8d54ef"');
-    await queryRunner.query('ALTER TABLE "company_admin" DROP COLUMN "company"');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "company" uuid');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD CONSTRAINT "UQ_3f916f97edcba428ad50d8d54ef" UNIQUE ("company")');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "company_id" uuid');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD CONSTRAINT "UQ_a50386174b5d9b1a258f2332cce" UNIQUE ("company_id")');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "phone" character varying NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "address" character varying NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "name" character varying NOT NULL');
-    await queryRunner.query('ALTER TABLE "company_admin" ADD "salary" integer NOT NULL');
-    await queryRunner.query(
-      'ALTER TABLE "company_admin" ADD CONSTRAINT "FK_3f916f97edcba428ad50d8d54ef" FOREIGN KEY ("company") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
-    );
-    await queryRunner.query(
-      'ALTER TABLE "company_admin" ADD CONSTRAINT "FK_a50386174b5d9b1a258f2332cce" FOREIGN KEY ("company_id") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
-    );
+    const query = `DELETE from public."user" WHERE id='2b510726-e10c-4efc-a2c9-6df536382ecb'`;
+    await queryRunner.query(query);
   }
 }
